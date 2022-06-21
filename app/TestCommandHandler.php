2f5ed3d09dce3fe9209f29184744b9d59a4d5c8c
subwayminder <subwayminder@gmail.com>
2022-06-21 13:20:32 +0300

    added one console command

--- app/TestCommandHandler.php
+++ app/TestCommandHandler.php
@@ -4,7 +4,39 @@
 namespace Subwayminder\ConsoleSample;
 
 
-class TestCommandHandler
-{
+use Subwayminder\Console\Commands\Command;
+use Subwayminder\Console\Commands\ICommandHandler;
+use Subwayminder\Console\Input\Input;
+use Subwayminder\Console\Output\Output;
+use Subwayminder\Console\Args\Argument;
 
+class TestCommandHandler implements ICommandHandler
+{
+    /**
+     * Простой обработчик для консольной команды. Выводит в консоль все переданные в консоль аргументы и опции
+     * @param Input $input
+     * @param Output $output
+     * @param Command $command
+     */
+    public static function handle(Input $input, Output $output, Command $command): void
+    {
+        /**
+         * @var Argument $argument
+         */
+        $output->writeLn(0, 'Command name' . $input->getCommand());
+
+        $output->writeLn(0, 'Arguments:');
+        foreach ($input->getArguments() as $argument){
+            $output->writeLn(3, '- '.$argument);
+        }
+
+        $output->writeLn(0, 'Options:');
+        foreach ($input->getParams() as $k=>$param)
+        {
+            $output->writeLn(3, '- '.$k);
+            foreach ($param as $value){
+                $output->writeLn(9, '- '.$value);
+            }
+        }
+    }
 }
--- bin/console.php
+++ bin/console.php
@@ -1,1 +1,34 @@
 <?php
+#!/usr/bin/env php
+if (file_exists($autoload = __DIR__.'/../../../autoload.php')) {
+    require_once $autoload;
+} else {
+    require_once __DIR__.'/../vendor/autoload.php';
+}
+
+use Subwayminder\Console\Input\ArgsInput;
+use Subwayminder\Console\Application\DefaultApplication;
+use Subwayminder\Console\Commands\Command;
+use Subwayminder\Console\Output\Output;
+use Subwayminder\ConsoleSample\TestCommandHandler;
+
+$input = new ArgsInput();
+//var_dump($argv);
+//var_dump($input->getArguments());
+//var_dump($input->getParams());
+
+$app = new DefaultApplication('sample_application', new ArgsInput(), new Output());
+$app
+    ->addCommand(
+        (new Command('test_command', 'A command for testing'))
+            ->addArgument('test', 'Some test argument')
+            ->addParam('value', 'Some value')
+    )
+    ->addCommand(
+        (new Command('test', 'A command for testing'))
+            ->addArgument('test', 'Some test argument')
+            ->addParam('value', 'Some value')
+            ->addHandler(new TestCommandHandler())
+    )
+;
+$app->run();
